4b7a1ffc14834d11d09371fc3d14963e1be46980
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2022-11-25 19:39:57 +0200

    Update 0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,4 +2,4 @@
 
 exec { 'fix config typo':
   command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin',
+  path    => '/usr/local/bin/:/bin/'
