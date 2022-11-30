beb22c81fb0ebd1eede8c018186eb706f52be6f8
oluwabunmi2 <o.adebanjo@alustudent.com>
2022-11-30 21:48:25 +0000

    sky is the limit

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
+#!/usr/bin/env bash
 # Testing how well our web server setup featuring Nginx is doing under pressure
 # fix stack so that it get to 0
-exec {'modify max open files limit setting':
-  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
-   path => shell,
+exec { 'nginx server':
+  command => "/bin/echo ulimit='-n 1000' | sudo tee /etc/default/nginx && sudo service nginx restart"
 }
